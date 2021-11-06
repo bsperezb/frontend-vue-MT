c717b28662ad7d6d149f8489b87b76e87c277a54
brayan <brayanperezbuitrago@gmail.com>
2021-11-06 17:33:37 -0500

    Agregado axios a Form.vue (created data)

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,32 +5,32 @@
     <div class=" row col-md-6 justify-content-center">
       <h2>Registrar producto</h2>
     <!-- </div> -->
-	  <form>
+	  <form v-on:submit.prevent="create_item">
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4">Nombre</label>
-      <input type="text" class="form-control" id="name" placeholder="Nombre">
+      <input type="text" v-model="item.name" class="form-control" id="name" placeholder="Nombre">
     </div>
   <div class="form-group col-md-6">
     <label for="inputAddress">Precio</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="precio">
+    <input type="text" v-model="item.price" class="form-control" id="inputAddress" placeholder="precio">
   </div>
   </div>
   <div class="form-row">
   <div class="form-group col-md-12">
     <label for="inputAddress2">Descripción</label>
-    <textarea type="text" class="form-control" id="description" rows="6" >  
+    <textarea type="text" v-model="item.description" class="form-control" id="description" rows="6" >  
       Descripción del producto
     </textarea>
   </div>
   <div class="form-group col-md-12">
     <label for="inputAddress">Url de la Imagen</label>
-    <input type="url" class="form-control" id="imagen del producto" placeholder="Price">
+    <input type="url" v-model="item.image" class="form-control" id="imagen del producto" placeholder="Price">
   </div>
   </div>
   <div class="form-group">
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <input class="form-check-input" v-model="item.state" type="checkbox" id="gridCheck">
       <label class="form-check-label" for="gridCheck" id="state">
         Activado
       </label>
@@ -45,3 +45,59 @@
  </div>
   </div>
 </template>
+<script>
+import axios from "axios";
+// import { response } from 'express';
+export default {
+  name: "Form",
+  data: function() {
+    return {
+      item: {
+        name: "",
+        state: "",
+        price: "",
+        description: "",
+        image: "",
+      },
+    };
+  },
+  methods: {
+    create_item: function() {
+      // axios.post("https://ciclo3-api-backend.herokuapp.com/login/", this.item, {
+      //     headers: {},
+      //   })
+      //   .then((result) => {
+      //     let dataLogIn = {
+      //       username: this.user.username,
+      //       token_access: result.data.access,
+      //       token_refresh: result.data.refresh,
+      //     };
+
+      //     this.$emit("completedLogIn", dataLogIn);
+      //   })
+      //   .catch((error) => {
+      //     if (error.response.status == "401")
+      //       alert("ERROR 401: Credenciales Incorrectas.");
+      //   });
+      // var axios = require('axios')
+      var data = this.item;
+      var config = {
+        method: 'post',
+        url: 'https://bcend.herokuapp.com/products/products/',
+        headers: {'Content-Type': 'application/json'},
+        data : data
+      };
+      console.log(this.item)
+      
+      axios(config)
+        .then(function (response){
+          console.log(JSON.stringify(response.data));
+          console.log('enviado')
+        })
+        .catch(function(error){
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
